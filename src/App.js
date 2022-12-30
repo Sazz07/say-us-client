759768423e73fa6a40699f79bf8b0d02409a42d4
Sazz07 <sazzad.hossain882@gmail.com>
2022-12-30 10:08:22 +0600

    Folder structure done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 
+import { Toaster } from 'react-hot-toast';
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import { router } from './Routes/Routes/Routes';
 
 function App() {
   return (
     <div className="">
-      <h1>YHer</h1>
+      <RouterProvider router={router}></RouterProvider>
+      <Toaster></Toaster>
     </div>
   );
 }
